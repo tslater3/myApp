8b80d34b556a2dc3c5494ba01c77caffafbf6433
tslater3 <trevor.s.slater@gmail.com>
2015-08-27 11:57:52 -0600

    second commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,29 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :author, :body) #this is like new attr_accessor
+  end
+end
